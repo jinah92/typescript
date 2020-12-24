dfd801e7fece48815663a3979f8357775c8c09a4
jinah <jinah0818@gmail.com>
2020-12-24 14:44:57 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-interface Human {
-   name: string;
-   age: number;
-   gender: string;
+class Human{
+   public name:string;
+   public age:number;
+   public gender:string;
+   constructor(name:string, age:number, gender:string){
+      // 생성자: 객체 생성시 마다 실행
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+   }
 }
 
-const person = {
-   name: 'jinah',
-   age: 29,
-   gender: 'female'
-}
+const person = new Human('jinah', 29, 'female')
+// const person = {
+//    name: 'jinah',
+//    age: 29,
+//    gender: 'female'
+// }
 
 const sayHi = (person: Human): string=>{
    return (`Hello ${person.name}, you're ${person.age} and ${person.gender}`)
