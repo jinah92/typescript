c97251759e509b20065108bdd3d7cd56c7522bbb
jinah <jinah0818@gmail.com>
2020-12-28 16:03:47 +0900

    createNewBlock 함수 작성

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as CryptoJS from 'crypto-js'
+import { create } from 'domain';
 
 class Block {
    public index:number;
@@ -23,7 +24,22 @@ const genesisBlock:Block = new Block(0, "123456211", "", "Hello Block", 123456);
 let blockchain: [Block] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
+
 const getLastBlock = (): Block => blockchain[blockchain.length - 1];
+
 const getNewTimestamp = ():number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string): Block => {
+   const previousBlock : Block = getLastBlock();
+   const newIndex : number = previousBlock.index + 1;
+   const nexTimestamp : number = getNewTimestamp();
+   const nextHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, nexTimestamp, data);
+
+   const newBlock : Block = new Block(newIndex, nextHash, previousBlock.hash, data, nexTimestamp);
+
+   return newBlock;
+}
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {}
