f9723501944d9a869ad2d8aa21ebb6de00b101a5
jinah <jinah0818@gmail.com>
2020-12-28 15:55:40 +0900

    make block

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-class Human{
-   public name:string;
-   public age:number;
-   public gender:string;
-   constructor(name:string, age:number, gender:string){
-      // 생성자: 객체 생성시 마다 실행
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
+import * as CryptoJS from 'crypto-js'
+
+class Block {
+   public index:number;
+   public hash: string;
+   public previousHash: string;
+   public data: string;
+   public timestamp: number;
+
+   static calculateBlockHash = (index:number, previousHash:string, timestamp: number, data:string): string=> CryptoJS.SHA256(index + previousHash +timestamp + data).toString();
+
+   constructor(index:number, hash: string, previousHash: string, data: string, timestamp: number){
+      this.index = index;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp
    }
 }
 
-const person = new Human('jinah', 29, 'female')
-// const person = {
-//    name: 'jinah',
-//    age: 29,
-//    gender: 'female'
-// }
+const genesisBlock:Block = new Block(0, "123456211", "", "Hello Block", 123456);
 
-const sayHi = (person: Human): string=>{
-   return (`Hello ${person.name}, you're ${person.age} and ${person.gender}`)
-} // optioinal: ?
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(person))
+const getBlockchain = (): Block[] => blockchain;
+const getLastBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimestamp = ():number => Math.round(new Date().getTime() / 1000);
 
 export {}
